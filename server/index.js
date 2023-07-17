3042684ded6b5f877fa77a99a04f6c5a3c0076dd
SameerSwankar <89686898+matrolat@users.noreply.github.com>
2023-07-17 18:50:20 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express, { Router } from 'express';
+import dotenv from 'dotenv';
+import connection from './database/db.js';
+import bodyParser from 'body-parser'
+import DefaultData from './default.js';
+import router from './routes/route.js';
+import cors from 'cors';
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',router)
+const PORT = 8000;
+
+// const username = process.env.DB_USERNAME;
+// const password = process.env.DB_PASSWORD;
+
+
+connection();
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+DefaultData();
